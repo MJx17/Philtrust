6c8124a547f00ded4716d22967d43f8a8f26af53
MJx17 <markjustin.borja17@gmail.com>
2024-10-09 15:19:30 +0800

    Update main.jsx

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// src/main.jsx or src/App.jsx
 
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Main = () => {
+  useEffect(() => {
+    // Matomo Tag Manager
+    var _mtm = window._mtm = window._mtm || [];
+    _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+    (function() {
+      var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+      g.async = true; 
+      g.src = 'http://localhost:8080/js/container_6MwLaDpK.js'; // Adjust the URL as needed
+      s.parentNode.insertBefore(g, s);
+    })();
+  }, []);
+
+  return <App />;
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
