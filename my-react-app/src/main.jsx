92a03b588d792aff58cb27495637232119294307
MJx17 <markjustin.borja17@gmail.com>
2024-10-10 10:35:18 +0800

    Update main.jsx

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,16 +1,16 @@
-// src/main.jsx or src/App.jsx
-
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const Main = () => {
-  React.useEffect(() => {
+  useEffect(() => {
     var _mtm = window._mtm = window._mtm || [];
     _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='https://cdn.matomo.cloud/philtrust.matomo.cloud/container_2X8pj0Zg.js'; s.parentNode.insertBefore(g,s);
-   }, [])
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.async = true; 
+    g.src = 'https://cdn.matomo.cloud/philtrust.matomo.cloud/container_2X8pj0Zg.js'; 
+    s.parentNode.insertBefore(g, s);
+  }, []);
 
   return <App />;
 };
